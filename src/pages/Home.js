fe15c258f2d8aa541256117d7f60744bc6d7a027
Dilan Silva <sdilansilva@gmail.com>
2021-03-03 12:59:25 +0530

    Article Input Area is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,23 @@
 import React,{Component} from 'react';
-import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavBar from '../components/NavBar'//import Navigation bar component
 const Home = () => {
     return(
         <div>
-            <NavBar/>
+            <NavBar/>{/*Navigation bar */}
 
+                <Form>{/*User Input for Input article here*/}
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>
+                                <h1>
+                                    Add Your Article Here
+                                </h1>
+                            </Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
         </div>
     );
 }
